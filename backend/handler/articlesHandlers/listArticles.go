fa4f500286151616b441b45bfa2eb3771c822ca5
Igor Afonso <igor.henrique.afonso99@gmail.com>
2025-09-16 22:21:02 -0300

    feat: finishing all get handler

--- backend/handler/articlesHandlers/listArticles.go
+++ backend/handler/articlesHandlers/listArticles.go
@@ -1,11 +1,29 @@
 package articleshandlers
 
 import (
-	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"main.go/schema"
 )
 
+// @BasePath /api/v1
+
+// @Summary List Articles
+// @Schemes
+// @Description List all articles
+// @Tags Article
+// @Accept json
+// @Produce json
+// @Success 200 {object} ListArticleResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /articles [get]
 func ListArticles(ctx *gin.Context) {
-	fmt.Println("FUNCIONOU ARTIGO")
+	article := []schema.Articles{}
+
+	if err := db.Find(&article).Error; err != nil{
+		sendErr(ctx, http.StatusInternalServerError, "error listing articles")
+		return
+	}
+	sendSucces(ctx, "list-articles", article)
 }
