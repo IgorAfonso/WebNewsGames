2a0f830c2449274e36b2ac2f42e7969d4bca7689
Igor Afonso <igor.henrique.afonso99@gmail.com>
2025-11-23 11:44:34 -0300

    feat: creating news crud endpoints

--- backend/src/WebGames/WebGames.API/Controllers/NewsController.cs
+++ backend/src/WebGames/WebGames.API/Controllers/NewsController.cs
@@ -6,18 +6,42 @@ namespace WebGames.API.Controllers;
 
 [Route("/api/v1/news")]
 [ApiController]
-public class NewsController(INewsAppService newsAppservice) : Controller
+public class NewsController(INewsAppService newsAppservice) : BaseController
 {
     private INewsAppService _newsAppservice = newsAppservice;
 
-    [HttpGet]
-    public async Task<IActionResult> GetByName([FromBody] NewsByNameRequest request)
+    [HttpGet("id")]
+    public async Task<IActionResult> GetByName([FromQuery] string request)
     {
-        if (request != null)
-        {
-            return Ok();
-        }
+        var appService = await _newsAppservice.GetByName(request);
+        return CustomResponse(appService.Item1, appService.Item2);
+    }
 
-        return BadRequest();
+    [HttpGet]
+    public async Task<IActionResult> GetById([FromQuery] Guid Id)
+    {
+        var appService = await _newsAppservice.GetById(Id);
+        return CustomResponse(appService.Item1, appService.Item2);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostNews([FromBody] PostNewsRequest request)
+    {
+        var appService = await _newsAppservice.PostNews(request);
+        return PostResponse(appService.Item1, appService.Item2);
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> DeleteNews([FromQuery] Guid Id)
+    {
+        var appService = await _newsAppservice.DeleteNews(Id);
+        return CustomResponse(appService.Item1, appService.Item2);
+    }
+
+    [HttpPatch]
+    public async Task<IActionResult> PatchNews([FromBody] PatchNewsRequest request)
+    {
+        var appService = await _newsAppservice.PatchNews(request);
+        return CustomResponse(appService.Item1, appService.Item2);
     }
 }
