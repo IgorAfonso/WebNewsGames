0f3e820ffebcb4d2a03d6f9469f9872b53316ead
Igor Afonso <igor.henrique.afonso99@gmail.com>
2025-11-23 11:43:55 -0300

    feat: creating base controller

---
+++ backend/src/WebGames/WebGames.API/Controllers/BaseController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebGames.API.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected ActionResult CustomResponse(bool success, object? response = null)
+        {
+            if (!success)
+            {
+                return BadRequest(new
+                {
+                    success = false,
+                    message = "Failed to process the request."
+                });
+            }
+
+            if(response != null)
+                return Ok(new
+                {
+                    success = true,
+                    message = "Success to process the request.",
+                    data = response
+                });
+
+            return Ok(new
+            {
+                success = true,
+                message = "Success to process the request."
+            });
+        }
+
+        protected ActionResult PostResponse(bool success, object? response = null)
+        {
+            if (!success)
+            {
+                return BadRequest(new
+                {
+                    success = false,
+                    message = "Failed to process the request."
+                });
+            }
+
+            if (response != null)
+                return Created(string.Empty, new
+                {
+                    success = true,
+                    message = "Success to process the request.",
+                    data = response
+                });
+
+            return Created(string.Empty, new
+            {
+                success = true,
+                message = "Success to process the request.",
+            });
+        }
+    }
+}
